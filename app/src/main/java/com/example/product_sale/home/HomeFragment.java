9daa69ceddd8d9faac1e4febf0fbcb5fc0d8101f
STHAOPHAN <phansongthao203@gmail.com>
2024-07-05 20:49:35 +0700

    update 5/7

--- app/src/main/java/com/example/product_sale/home/HomeFragment.java
+++ app/src/main/java/com/example/product_sale/home/HomeFragment.java
@@ -1,44 +1,67 @@
 package com.example.product_sale.home;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.product_sale.R;
+import com.example.product_sale.activity.CartActivity;
+import com.example.product_sale.activity.HomeActivity;
+import com.example.product_sale.activity.LoginActivity;
 import com.example.product_sale.adapter.PetAdapter;
 import com.example.product_sale.databinding.FragmentHomeBinding;
+import com.example.product_sale.models.Cart;
 import com.example.product_sale.models.Pet;
+import com.example.product_sale.service.PetApiService;
 import com.example.product_sale.utils.MyDatabaseHelper;
+import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
-    private RecyclerView recyclerView;
     private PetAdapter petAdapter;
-    private List<Pet> petList;
-    private MyDatabaseHelper databaseHelper;
+    private RecyclerView rvPets;
+    private List<Pet> mListPet;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-/*        recyclerView = binding.petList;
-        recyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
+        rvPets = binding.rvPets;
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+        rvPets.setLayoutManager(linearLayoutManager);
 
-        databaseHelper = new MyDatabaseHelper(requireContext());
-        databaseHelper.addSamplePets();
-        petList = databaseHelper.getAllPets();
+        DividerItemDecoration itemDecoration = new DividerItemDecoration(getActivity(), DividerItemDecoration.VERTICAL);
+        rvPets.addItemDecoration(itemDecoration);
 
-        petAdapter = new PetAdapter(petList);
-        recyclerView.setAdapter(petAdapter);*/
+        mListPet = new ArrayList<>();
+        petAdapter = new PetAdapter(getActivity(), mListPet, Cart.getInstance());
+        rvPets.setAdapter(petAdapter);
+        callApiGetPets();
         return root;
     }
 
@@ -46,5 +69,49 @@ public class HomeFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    private void callApiGetPets() {
+        PetApiService petApiService = PetApiService.retrofit.create(PetApiService.class);
+        petApiService.getPets().enqueue(new Callback<List<Pet>>() {
+            @Override
+            public void onResponse(Call<List<Pet>> call, Response<List<Pet>> response) {
+                if (response.isSuccessful()) {
+                    mListPet.clear(); // Xóa danh sách cũ
+                    mListPet.addAll(response.body()); // Thêm danh sách mới từ response
+                    petAdapter.notifyDataSetChanged(); // Cập nhật RecyclerView
+                } else {
+                    Toast.makeText(getActivity(), "Failed to get pets", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Pet>> call, Throwable t) {
+                Log.e("API_ERROR", "Failure: " + t.getMessage());
+                Toast.makeText(getActivity(), "Network error", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_home, menu); // Inflate the menu
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.action_home_cart) {
+            Intent intent = new Intent(getActivity(), CartActivity.class);
+            intent.putParcelableArrayListExtra("cartItems", new ArrayList<>(Cart.getInstance().getCartItems()));
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
